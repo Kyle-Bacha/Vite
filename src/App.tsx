1ed2c2473e1b7fb8fb0525782083584889654321
Kyle-Bacha <kybacha1@gmail.com>
2023-04-07 11:27:48 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import ListGroup from './Components/ListGroup';
+
+function App(){
+  let items = ['New York', 'San Francisco', 'Tokyo', 'Beijing', 'London']; 
+  return <div><ListGroup items={items} heading="m ,234eqa=_){p(8"/></div>
+}
+
+export default App;
